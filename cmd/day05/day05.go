1377dc4612a1c452a31eccb504095270f0e93fe7
Fredrik Lönnblad <fredrik+github@lonnblad.com>
2023-12-06 06:12:01 +0100

    Solution for Day Five (#6)

---
+++ cmd/day05/day05.go
@@ -0,0 +1,87 @@
+package day05
+
+import (
+	_ "embed"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+//go:embed real.input
+var realInput string
+
+func mapToSoils(input string, seeds []int) []int {
+	return mapTo(input, "seed-to-soil map:\n", seeds)
+}
+
+func mapToFertilizers(input string, seeds []int) []int {
+	return mapTo(input, "soil-to-fertilizer map:\n", seeds)
+}
+
+func mapToWaters(input string, seeds []int) []int {
+	return mapTo(input, "fertilizer-to-water map:\n", seeds)
+}
+
+func mapToLights(input string, seeds []int) []int {
+	return mapTo(input, "water-to-light map:\n", seeds)
+}
+
+func mapToTemperatures(input string, seeds []int) []int {
+	return mapTo(input, "light-to-temperature map:\n", seeds)
+}
+
+func mapToHumidities(input string, seeds []int) []int {
+	return mapTo(input, "temperature-to-humidity map:\n", seeds)
+}
+
+func mapToLocations(input string, seeds []int) []int {
+	return mapTo(input, "humidity-to-location map:\n", seeds)
+}
+
+func mapTo(input, key string, sources []int) []int {
+	input = strings.Split(input, key)[1]
+	numbers := strings.SplitN(input, "\n\n", 2)[0]
+
+	pattern := regexp.MustCompile(`(\d+) (\d+) (\d+)`)
+	rows := strings.Split(numbers, "\n")
+
+	mappers := make([]interval, len(rows))
+
+	for idx, row := range rows {
+		matches := pattern.FindStringSubmatch(row)[1:]
+
+		mappers[idx].destStart, _ = strconv.Atoi(matches[0])
+		mappers[idx].sourceStart, _ = strconv.Atoi(matches[1])
+		mappers[idx].rangeLength, _ = strconv.Atoi(matches[2])
+	}
+
+	result := make([]int, len(sources))
+
+	for idx, source := range sources {
+		val, has := hasMapping(mappers, source)
+		if has {
+			result[idx] = val
+			continue
+		}
+
+		result[idx] = source
+	}
+
+	return result
+}
+
+type interval struct {
+	destStart   int
+	sourceStart int
+	rangeLength int
+}
+
+func hasMapping(mappers []interval, source int) (val int, found bool) {
+	for _, interval := range mappers {
+		if source >= interval.sourceStart && source < interval.sourceStart+interval.rangeLength {
+			return interval.destStart + (source - interval.sourceStart), true
+		}
+	}
+
+	return 0, false
+}
---
+++ cmd/day05/part01_test.go
@@ -0,0 +1,25 @@
+package day05_test
+
+import (
+	_ "embed"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/lonnblad/advent-of-code-2023/cmd/day05"
+)
+
+func Test_ExampleInput(t *testing.T) {
+	expectedOutput := []int{82, 43, 86, 35}
+	expectedSum := 35
+
+	actualOutput, actualSum := day05.SolvePartOneForInput(exampleInput)
+	assert.Equal(t, expectedOutput, actualOutput)
+	assert.Equal(t, expectedSum, actualSum)
+}
+
+func Test_RealInput(t *testing.T) {
+	expectedSum := 535088217
+	actualSum := day05.SolvePartOneForRealInput()
+	assert.Equal(t, expectedSum, actualSum)
+}
